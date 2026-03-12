12a110e5b5fa91e2079d1358058d7791d51ffd03
AniruddhaChaudhury2006 <chaudhuryaniruddha56@gmail.com>
2026-03-12 20:18:33 +0530

    Update App_Rock_Vs_Mine.py

--- App_Rock_Vs_Mine.py
+++ App_Rock_Vs_Mine.py
@@ -7,7 +7,7 @@ import shap
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 st.set_page_config(page_title = "Rock Vs Mine Prediction System", layout = 'wide')
-st.markdown("""<style> body { background-color: #0E1117; } .stApp {  background: linear-gradient(135deg,#0f2027,#203a43,#2c5364); color:white; </style>""", unsafe_allow_html = True})
+st.markdown("""<style> body { background-color: #0E1117; } .stApp {  background: linear-gradient(135deg,#0f2027,#203a43,#2c5364); color:white; </style>""", unsafe_allow_html = True)
 st.title("🔎 🧠 Futuristic Rock vs Mine AI Detection System")
 sonar_data = pd.read_csv('/content/sonar dataset.csv', header = None)
 X = sonar_data.drop(columns=60, axis=1)
