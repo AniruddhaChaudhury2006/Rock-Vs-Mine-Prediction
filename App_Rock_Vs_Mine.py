de3c158c8520636e3d5c79c381fe184dc6354d72
AniruddhaChaudhury2006 <chaudhuryaniruddha56@gmail.com>
2026-03-12 21:22:56 +0530

    Update App_Rock_Vs_Mine.py

--- App_Rock_Vs_Mine.py
+++ App_Rock_Vs_Mine.py
@@ -177,9 +177,7 @@ with tab1:
     ]
 )
 
-    fig = go.Figure(data=[go.Scatterpolar(r = input_data, theta = theta, mode = "lines", line = dict(color = "lime", width = 3))], frames = frames)
-    fig.update_layout(polar = dict(radialaxis = dict(visible = True, range = [0, 1])), showlegend = False, title = "Animated Sonar Radar Sweep", updatemenus = [dict(type = "buttons", buttons = [dict(label = "Start Scan", method = 'animate', args = [None])])])
-    st.plotly_chart(fig, use_container_width=True)
+    
 
 
 
