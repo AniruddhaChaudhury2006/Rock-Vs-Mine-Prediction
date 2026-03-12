d6d51d85a596b242bffb7b01f124c673f8f53e26
AniruddhaChaudhury2006 <chaudhuryaniruddha56@gmail.com>
2026-03-12 20:27:06 +0530

    Update App_Rock_Vs_Mine.py

--- App_Rock_Vs_Mine.py
+++ App_Rock_Vs_Mine.py
@@ -9,7 +9,7 @@ from sklearn.linear_model import LogisticRegression
 st.set_page_config(page_title = "Rock Vs Mine Prediction System", layout = 'wide')
 st.markdown("""<style> body { background-color: #0E1117; } .stApp {  background: linear-gradient(135deg,#0f2027,#203a43,#2c5364); color:white; </style>""", unsafe_allow_html = True)
 st.title("🔎 🧠 Futuristic Rock vs Mine AI Detection System")
-sonar_data = pd.read_csv('sonar dataset.csv', header = None)
+sonar_data = pd.read_csv('sonar_dataset.csv', header = None)
 X = sonar_data.drop(columns=60, axis=1)
 Y = sonar_data[60]
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1, stratify = Y, random_state = 25)
@@ -40,7 +40,7 @@ if st.sidebar.button("Predict"):
   fig = go.Figure(go.Indicator(mode = 'gauge + number', value = mine_prob * 100, title = {'text' : 'Mine probability'}, gauge = {'axis' : {'range': [0, 100]}}))
   st.plotly_chart(fig, use_container_width = True)
   radar = go.Figure()
-  radar.add_trace(go.Scatterpolar(r = inputs[:10], theta = [f"S{i + 1}" for i in range(10)], fill = 'toself', name = 'Signals'))
+  radar.add_trace(go.Scatterpolar(r = input_data[:10], theta = [f"S{i + 1}" for i in range(10)], fill = 'toself', name = 'Signals'))
   radar.update_layout(polar = dict(radialaxis = dict(visible = True)), showlegend = False, title = "Sonar Signal Radar Chart")
   st.plotly_chart(radar, use_container_width = True)
 st.subheader("🧠 SHAP Explainable AI")
